fe3a726c2282f01d67d7595114066ae98213a5fb
Fathan <74785057+Churma16@users.noreply.github.com>
2026-01-05 22:16:30 +0700

    feat: add Layout component to Home page for improved structure and styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,53 @@
 import { useProjects } from "../hooks/useProjects";
 import ProjectCard from "../components/projects/ProjectCard";
+import Layout from "../components/layout/layout";
 
 function App() {
     // Panggil hook sakti kita
     const { data: projects, isLoading, isError, error } = useProjects();
 
     return (
-        <div className="min-h-screen bg-dark-bg p-10 font-sans text-slate-100">
-            <div className="mx-auto max-w-6xl">
-                <header className="mb-12 text-center">
-                    <h1 className="text-4xl font-bold mb-4">
-                        My <span className="text-brand">Portfolio</span>
-                    </h1>
-                    <p className="text-slate-400">
-                        Built with React, Laravel & Tailwind
-                    </p>
-                </header>
+        <Layout>
+            <div className="min-h-dvh bg-linear-to-b from-cyan-200 to-white to-[60vh]">
+                <div className="mx-auto max-w-6xl">
+                    <header className="mb-12 text-center">
+                        <h1 className="text-4xl font-bold mb-4">
+                            My <span className="text-brand">Portfolio</span>
+                        </h1>
+                        <p className="text-slate-400">
+                            Built with React, Laravel & Tailwind
+                        </p>
+                    </header>
 
-                {/* --- STATE HANDLING --- */}
+                    {/* --- STATE HANDLING --- */}
 
-                {/* 1. Loading State */}
-                {isLoading && (
-                    <div className="text-center py-20 animate-pulse text-slate-500">
-                        Loading Projects from Laravel...
+                    {/* 1. Loading State */}
+                    {isLoading && (
+                        <div className="text-center py-20 animate-pulse text-slate-500">
+                            Loading Projects from Laravel...
+                        </div>
+                    )}
+
+                    {/* 2. Error State */}
+                    {isError && (
+                        <div className="text-center py-20 text-red-400 bg-red-900/10 rounded-lg border border-red-900">
+                            Failed to fetch data. <br />
+                            <span className="text-sm opacity-70">
+                                {(error as Error).message} (Check your Laravel
+                                Server!)
+                            </span>
+                        </div>
+                    )}
+                    {/* <pre>{JSON.stringify(projects, null, 2)}</pre> */}
+                    {/* 3. Success State (Grid) */}
+                    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+                        {projects?.map((project) => (
+                            <ProjectCard key={project.id} project={project} />
+                        ))}
                     </div>
-                )}
-
-                {/* 2. Error State */}
-                {isError && (
-                    <div className="text-center py-20 text-red-400 bg-red-900/10 rounded-lg border border-red-900">
-                        Failed to fetch data. <br />
-                        <span className="text-sm opacity-70">
-                            {(error as Error).message} (Check your Laravel
-                            Server!)
-                        </span>
-                    </div>
-                )}
-
-                {/* 3. Success State (Grid) */}
-                <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-                    {projects?.map((project) => (
-                        <ProjectCard key={project.id} project={project} />
-                    ))}
                 </div>
             </div>
-        </div>
+        </Layout>
     );
 }
 
