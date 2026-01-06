bb4bc0326f5cd114467ad4c1616c97bb95b7ee54
Fathan <74785057+Churma16@users.noreply.github.com>
2026-01-07 02:29:02 +0700

    feat: add routes for Categories and Profile in App component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,9 @@ import DashboardLayout from "./components/layout/DashboardLayout"; // Kita buat 
 import ProjectList from "./pages/admin/ProjectList"; // Halaman CRUD nanti
 import TechStackList from "./pages/admin/masters/TechStackList";
 import TagList from "./pages/admin/masters/TagList";
+import CategoryList from "./pages/admin/masters/CategoryList";
 import SetToken from "./pages/SetToken";
+import ProfileForm from "./pages/admin/ProfileForm";
 
 function App() {
     return (
@@ -30,6 +32,8 @@ function App() {
                         <Route index element={<ProjectList />} />
                         <Route path="tech-stacks" element={<TechStackList />} />
                         <Route path="tags" element={<TagList />} />
+                        <Route path="categories" element={<CategoryList />} />
+                        <Route path="profile" element={<ProfileForm />} />
                         {/* Nanti tambah route lain di sini: Create, Edit, dll */}
                     </Route>
                 </Route>
