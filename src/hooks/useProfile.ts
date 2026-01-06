f6cfd0542ce7361d0647bbb08ff4a04f52ff41c9
Fathan <74785057+Churma16@users.noreply.github.com>
2026-01-07 02:28:20 +0700

    refactor: rename fetchProfiles to fetchProfile and update related hooks

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,19 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../api/axios";
-import { Project, ApiResponse, Profile } from "../types";
+import { ApiResponse, Profile } from "../types";
 
-const fetchProfiles = async (): Promise<Profile> => {
-    // Ambil response dari Axios
+const fetchProfile = async (): Promise<Profile> => {
     const response = await apiClient.get<ApiResponse<Profile>>("/profiles");
-    
-    console.log("Profiles response:", response.data);
+
+    console.log("Profile response:", response.data);
     return response.data.data;
 };
 
-export const useProfiles = () => {
+export const useProfile = () => {
     return useQuery({
-        queryKey: ["profiles"],
-        queryFn: fetchProfiles,
+        queryKey: ["profile"],
+        queryFn: fetchProfile,
         staleTime: 1000 * 60 * 5,
     });
 };
