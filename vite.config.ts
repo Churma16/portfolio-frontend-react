6baf3c31a33fb9d29a98068b3baae0d677309d7f
Fathan <74785057+Churma16@users.noreply.github.com>
2026-01-06 17:14:45 +0700

    feat: update tsconfig and vite config for improved path resolution and aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['react-icons/si'],
-  },
-  build: {
-    commonjsOptions: {
-      include: [/react-icons[\\/]si/, /node_modules/],
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-})
+    optimizeDeps: {
+        include: ["react-icons/si"],
+    },
+    build: {
+        commonjsOptions: {
+            include: [/react-icons[\\/]si/, /node_modules/],
+        },
+    },
+});
