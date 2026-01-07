614fd4cabb5a49fe243cc364c3c7864bb19ee967
Fathan <74785057+Churma16@users.noreply.github.com>
2026-01-08 02:26:42 +0700

    feat: implement custom hooks for fetching and creating messages

---
+++ src/hooks/useMessages.ts
@@ -0,0 +1,44 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import apiClient from "../api/axios";
+import { ApiResponse, Message } from "../types";
+
+const fetchMessage = async (): Promise<Message> => {
+    const response = await apiClient.get<ApiResponse<Message>>("/messages");
+
+    console.log("Message response:", response.data);
+    return response.data.data;
+};
+
+interface CreateMessagePayload {
+    name: string;
+    email: string;
+    message: string;
+}
+
+export const useMessage = () => {
+    return useQuery({
+        queryKey: ["message"],
+        queryFn: fetchMessage,
+        staleTime: 1000 * 60 * 5,
+    });
+};
+
+export const useCreateMessage = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async (payload: CreateMessagePayload) => {
+            const response = await apiClient.post<ApiResponse<Message>>(
+                "/messages",
+                payload
+            );
+            return response.data.data;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["message"] });
+        },
+        onError: (err: any) => {
+            console.error("Error sending message:", err);
+        },
+    });
+};
