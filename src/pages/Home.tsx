d8946339b5cbd019a95da55aa497f2bb0a3938b2
Fathan <74785057+Churma16@users.noreply.github.com>
2026-01-08 15:16:11 +0700

    feat: refactor Home layout and add TechSection component with TechMarquee

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,18 @@
-import { useProjects } from "../hooks/useProjects";
-import ProjectCard from "../components/projects/ProjectCard";
-import Layout from "../components/layout/Layout";
-import Hero from "../components/layout/Hero";
-import TechMarquee from "../components/layout/TechMarquee";
-import { useTechStacks } from "../hooks/useTechStacks";
-import AboutSection from "../components/layout/AboutSection";
-import ArchitectureSection from "../components/layout/ArchitectureSection";
-import ServicesSection from "../components/layout/ServiceSection";
-import ContactSection from "../components/layout/ContactSection";
-import ProjectSection from "../components/layout/ProjectSection";
+import Layout from "../components/layout/Home/Layout.tsx";
+import Hero from "../components/layout/Home/Hero/HeroSection.tsx";
+import AboutSection from "../components/layout/Home/About/AboutSection.tsx";
+import ArchitectureSection from "../components/layout/Home/Architecture/ArchitectureSection.tsx";
+import ServicesSection from "../components/layout/Home/ServiceSection.tsx";
+import ContactSection from "../components/layout/Home/Contact/ContactSection.tsx";
+import ProjectSection from "../components/layout/Home/ProjectSection.tsx";
+import TechSection from "@/components/layout/Home/TechStacks/TechSection.tsx";
 
 function App() {
-    // Panggil hook sakti kita
 
     return (
         <Layout>
             <Hero />
-            <div className="m-24">
-                <div className="text-center max-w-3xl mx-auto mb-16 space-y-4">
-                    <h2 className="text-3xl md:text-4xl font-heading font-bold text-white">
-                        The Tech <span className="text-lara-blue"> Stack</span>.
-                    </h2>
-                    <p className="text-slate-400 text-lg">
-                        <span className="text-white font-medium">
-                        </span>
-                        The tools I use to build fast, scalable, and robust
-                        applications.
-                    </p>
-                </div>
-                {/* <pre>{JSON.stringify(techStacks, null, 2)}</pre> */}
-                <TechMarquee />
-            </div>
+            <TechSection/>
             <ProjectSection />
             <AboutSection />
             <ServicesSection />
